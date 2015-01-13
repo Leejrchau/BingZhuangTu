2c58bd83708a4bdb26691dbaf046d879d1f5862d
lizhichao <891998346@qq.com>
2015-01-13 16:16:19 +0800

    Initial Commit

---
+++ BingZhuangTu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BingZhuangTu
+//
+//  Created by fuliang123 on 15/1/13.
+//  Copyright (c) 2015年 lizhichao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BingZhuangTu/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BingZhuangTu
+//
+//  Created by fuliang123 on 15/1/13.
+//  Copyright (c) 2015年 lizhichao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
